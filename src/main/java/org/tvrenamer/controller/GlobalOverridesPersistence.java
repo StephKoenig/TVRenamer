6cdb2889ba4b8f25cc6ac1132f95251dfdd17076
Stephane Koenig <stephane.koenig@gmail.com>
2026-01-14 11:23:47 +0000

    Modernize XStream persistence IO and error handling

--- src/main/java/org/tvrenamer/controller/GlobalOverridesPersistence.java
+++ src/main/java/org/tvrenamer/controller/GlobalOverridesPersistence.java
@@ -2,21 +2,24 @@ package org.tvrenamer.controller;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.converters.reflection.PureJavaReflectionProvider;
-import org.tvrenamer.model.GlobalOverrides;
-
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.tvrenamer.model.GlobalOverrides;
 
 public class GlobalOverridesPersistence {
-    private static final Logger logger = Logger.getLogger(GlobalOverridesPersistence.class.getName());
+
+    private static final Logger logger = Logger.getLogger(
+        GlobalOverridesPersistence.class.getName()
+    );
 
     // Use reflection provider so the default constructor is called, thus calling the superclass constructor
-    private static final XStream xstream = new XStream(new PureJavaReflectionProvider());
+    private static final XStream xstream = new XStream(
+        new PureJavaReflectionProvider()
+    );
 
     static {
         xstream.alias("overrides", GlobalOverrides.class);
@@ -25,45 +28,66 @@ public class GlobalOverridesPersistence {
     /**
      * Save the overrides object to the file.
      *
-     * @param overrides
-     *            the overrides object to save
-     * @param path
-     *            the path to save it to
+     * @param overrides the overrides object to save
+     * @param path the path to save it to
      */
     @SuppressWarnings("SameParameterValue")
     public static void persist(GlobalOverrides overrides, Path path) {
         String xml = xstream.toXML(overrides);
 
-        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
-            writer.write(xml);
+        try {
+            Path parent = path.getParent();
+            if (parent != null) {
+                Files.createDirectories(parent);
+            }
 
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Exception occurred when writing overrides file", e);
+            // Overwrite any existing file
+            Files.writeString(path, xml);
+        } catch (
+            IOException
+            | UnsupportedOperationException
+            | SecurityException e
+        ) {
+            logger.log(
+                Level.SEVERE,
+                "Exception occurred when writing overrides file '" +
+                    path.toAbsolutePath() +
+                    "'",
+                e
+            );
         }
     }
 
     /**
      * Load the overrides from path.
      *
-     * @param path
-     *            the path to read
+     * @param path the path to read
      * @return the populated overrides object
      */
     @SuppressWarnings("SameParameterValue")
     public static GlobalOverrides retrieve(Path path) {
-        if (Files.exists(path)) {
-            try (InputStream in = Files.newInputStream(path)) {
-                return (GlobalOverrides) xstream.fromXML(in);
-            } catch (IllegalArgumentException | UnsupportedOperationException
-                     | IOException  | SecurityException e)
-            {
-                logger.log(Level.SEVERE, "Exception reading overrides file '"
-                           + path.toAbsolutePath().toString(), e);
-                logger.info("assuming no overrides");
-            }
+        if (Files.notExists(path)) {
+            // If file doesn't exist, assume defaults
+            logger.fine(
+                "Overrides file '" +
+                    path.toAbsolutePath() +
+                    "' does not exist - assuming no overrides"
+            );
+            return null;
         }
 
-        // If file doesn't exist, assume defaults
-        return null;
+        try (InputStream in = Files.newInputStream(path)) {
+            return (GlobalOverrides) xstream.fromXML(in);
+        } catch (IOException | IllegalArgumentException | SecurityException e) {
+            logger.log(
+                Level.SEVERE,
+                "Exception reading overrides file '" +
+                    path.toAbsolutePath() +
+                    "'",
+                e
+            );
+            logger.info("assuming no overrides");
+            return null;
+        }
     }
 }
