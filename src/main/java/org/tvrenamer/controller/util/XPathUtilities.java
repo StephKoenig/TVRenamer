544b40f063d19a6213dfcbf6f6f896ec85ba162a
Steph Koenig <stephane.koenig@gmail.com>
2026-01-17 02:36:00 +0000

    Make XPathUtilities thread-safe with ThreadLocal XPath

--- src/main/java/org/tvrenamer/controller/util/XPathUtilities.java
+++ src/main/java/org/tvrenamer/controller/util/XPathUtilities.java
@@ -1,32 +1,40 @@
 package org.tvrenamer.controller.util;
 
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class XPathUtilities {
 
-    private static final XPathFactory XPATH_FACTORY = XPathFactory.newInstance();
-    // We just create this one XPath object and use it for all shows, all episodes.
-    // TODO: is there any issue with this?
-    private static final XPath STD_XPATH = XPATH_FACTORY.newXPath();
+    private static final XPathFactory XPATH_FACTORY =
+        XPathFactory.newInstance();
+
+    /**
+     * XPath instances are not guaranteed to be thread-safe. Use a ThreadLocal
+     * instance so callers can safely evaluate XPath expressions from background
+     * threads (e.g., threaded preload / downloads).
+     */
+    private static final ThreadLocal<XPath> XPATH = ThreadLocal.withInitial(
+        () -> XPATH_FACTORY.newXPath()
+    );
+
+    private static XPath getXPath() {
+        return XPATH.get();
+    }
 
     public static NodeList nodeListValue(String name, Node eNode)
-        throws XPathExpressionException
-    {
-        XPathExpression expr = STD_XPATH.compile(name);
+        throws XPathExpressionException {
+        XPathExpression expr = getXPath().compile(name);
         return (NodeList) expr.evaluate(eNode, XPathConstants.NODESET);
     }
 
     public static String nodeTextValue(String name, Node eNode)
-        throws XPathExpressionException
-    {
-        XPathExpression expr = STD_XPATH.compile(name);
+        throws XPathExpressionException {
+        XPathExpression expr = getXPath().compile(name);
         Node node = (Node) expr.evaluate(eNode, XPathConstants.NODE);
         if (node == null) {
             return null;
